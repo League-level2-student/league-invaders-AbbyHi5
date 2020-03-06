3044ecc20ac98d6d853b286ccd98c59913b7c7ac
codeclub <codeclub@smms-rm23-21.smusd.local>
2020-03-06 10:33:34 -0800

    First Day

---
+++ src/GamePanel.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+	GamePanel game = new GamePanel();
+	JPanel panel = new JPanel();
+	
+	
+
+	
+	@Override
+	public void paintComponent(Graphics g){
+
+	}
+	
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
 
+	JFrame frame = new JFrame("League Invaders");
+	//JPanel panel = new JPanel();
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 500;
+	static LeagueInvaders leauge = new LeagueInvaders();
+	
+	public static void main(String[] args) {
+		
+		leauge.setup();
+		
+	}
+	
+	void setup() {
+		frame.add(game);
+		
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		
+	}
+	
 }
